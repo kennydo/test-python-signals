5f29b3edd367f1aa17be89cf446e69bb4e038e59
Kenny Do <kedo@patreon.com>
2016-09-21 15:50:05 -0700

    Trap SIGTERM and send SIGUSR1 instead!

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+WORKER_PID=''
 
-python3 /code/run_python_workers.py
+send_usr1_to_worker(){
+    kill -USR1 $WORKER_PID
+    wait $WORKER_PID
+}
+
+trap 'send_usr1_to_worker' TERM INT
+
+python3 /code/run_python_workers.py & WORKER_PID=$!
+wait $WORKER_PID
